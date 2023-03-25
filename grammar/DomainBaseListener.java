f14790e5209e8fd07f7e18ff62e0648a3b71cf9d
Paul Bowler <spbowler@gmail.com>
2023-03-25 10:24:04 +0100

    added build file

--- grammar/DomainBaseListener.java
+++ grammar/DomainBaseListener.java
@@ -1,4 +1,4 @@
-// Generated from Domain.g4 by ANTLR 4.12.0
+// Generated from ./grammar/Domain.g4 by ANTLR 4.12.0
 
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ErrorNode;
